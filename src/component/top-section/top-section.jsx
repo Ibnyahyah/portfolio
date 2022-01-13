abfa40b86e65697e5efc9a0ceb1de85402e6fbae
ibn2085 <yahyahridwan665@gmail.com>
2022-01-13 12:31:05 +0545

    adding resume

--- src/component/top-section/top-section.jsx
+++ src/component/top-section/top-section.jsx
@@ -7,7 +7,7 @@ function TopSection(){
             </div>
             <div className="mt-2">
                 <p>I am here to satisfy your needs in <b>Front-End</b> development.</p>
-                <p>Below are some tips about my Stacks and Projects. You can also <a href="mailto:yahyahridwan665@gamil.com" className="text-blue">Click here</a> to hire me.</p>
+                <p>Below are some tips about my Stacks and Projects. Download My <a href="https://docs.google.com/document/d/1xxhSJffJCym8ZaGXSBUcnOwgNAnav3ZOhQeH_aRNmpo/edit?usp=sharing" target="_blank" rel="noreferrer" className="text-blue">Resume</a>.</p>
             </div>
             <a href="#project" className="btn-outlined-blue mt-3 text-blue">Learn more</a>
         </div>
