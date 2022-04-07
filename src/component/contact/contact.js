904a956ac9715fc953126dda2fd4db6768b76165
ibnyahyah <yahyahridwan665@gmail.com>
2022-04-07 14:26:12 -0700

    adding firebase hosting and database

--- src/component/contact/contact.js
+++ src/component/contact/contact.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { useState } from "react";
 const Data = [
   {
@@ -8,9 +9,9 @@ const Data = [
   },
   {
     icon: "far fa-envelope",
-    text: "Gmail",
-    url: "mailto:yahyahridwan665@gmail.com",
-    bg: "#DB4437",
+    text: "Twitter",
+    url: "https://twitter.com/whitecode_",
+    bg: "#00acee",
   },
   {
     icon: "fab fa-github",
@@ -22,10 +23,29 @@ const Data = [
 
 const Contact = () => {
   const [contacts, setContact] = useState(false);
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [res, setRes] = useState("");
 
   function contactHandler() {
     setContact(!contacts);
   }
+
+  const data = {
+    email: email,
+    message: message,
+  };
+  const contactForm = async (e) => {
+    e.preventDefault();
+    await axios.post(
+      "https://myportfolio-d5709-default-rtdb.firebaseio.com/data.json",
+      data
+    );
+    setRes("Your Message Was SuccessFull");
+    setTimeout(()=>{
+      setContact(false);
+    },1200)
+  };
   return (
     <div className="container contact mt-2">
       <button className="btn-blue" onClick={contactHandler}>
@@ -35,10 +55,29 @@ const Contact = () => {
         {contacts && (
           <div className="card bg-white col-6-sm gap-1">
             <div className="col-6-sm col-6-md">
-              <p className="font-sm mb-1">You can Contact or Hire Me using the Below process:</p>
-              <form>
-                <input type="text" className="mb-1" placeholder="example@gmail.com" />
-                <textarea rows="5" placeholder="I will like to"></textarea>
+              <p className="font-sm mb-1">
+                You can Contact or Hire Me using the Below process:
+              </p>
+              <p className="text-red">{res}</p>
+              <form onSubmit={contactForm}>
+                <div>
+                  <input
+                    type="text"
+                    className="mb-1"
+                    placeholder="example@gmail.com"
+                    onChange={(e) => {
+                      setEmail(e.target.value);
+                    }}
+                  />
+                  <textarea
+                    rows="5"
+                    placeholder="I will like to"
+                    onChange={(e) => {
+                      setMessage(e.target.value);
+                    }}
+                  ></textarea>
+                </div>
+                <button>Submit</button>
               </form>
             </div>
             <div className="col-6-sm col-6-md">
@@ -46,7 +85,10 @@ const Contact = () => {
                 return (
                   <p className="mt-2" key={icon}>
                     <a href={url} target="_blank" rel="noreferrer">
-                      <i className={`${icon} ${bg} text-white p-1 br-full`} style={{backgroundColor:bg}}></i>
+                      <i
+                        className={`${icon} ${bg} text-white p-1 br-full`}
+                        style={{ backgroundColor: bg }}
+                      ></i>
                     </a>{" "}
                     Via {text}
                   </p>
